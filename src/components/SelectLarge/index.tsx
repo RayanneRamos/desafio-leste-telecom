25b7ac46ae26c7e464c0ae7fb8e47a33dafbfb33
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-01 20:30:00 -0300

    :wastebasket: remove: remove select large

--- src/components/SelectLarge/index.tsx
+++
@@ -1,8 +0,0 @@
-import { SelectHTMLAttributes } from "react";
-import styles from "./styles.module.scss";
-
-interface SelectLargeProps extends SelectHTMLAttributes<HTMLSelectElement> {}
-
-export function SelectLarge({ ...props }: SelectLargeProps) {
-  return <select className={styles.selectLarge} {...props}></select>;
-}
