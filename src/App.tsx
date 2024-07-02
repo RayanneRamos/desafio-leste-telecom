23e24df759656970d59d044fe02b189212dd7064
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-01 21:21:15 -0300

    :sparkles: feat: creating application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-//import { Home } from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 import { ListContacts } from "./pages/ListContacts";
-//import { NewContact } from "./pages/NewContact";
+import { NewContact } from "./pages/NewContact";
 
 export function App() {
-  return <ListContacts />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/addnewcontact" element={<NewContact />} />
+        <Route path="/listcontact" element={<ListContacts />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
