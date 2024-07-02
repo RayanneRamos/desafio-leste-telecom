f886111af98de3af00ade1c0857185218b727ea0
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-02 15:25:12 -0300

    :sparkles: create a languages mock

---
+++ src/utils/languages.ts
@@ -0,0 +1,734 @@
+export const LANGUAGES = [
+  {
+    id: "1",
+    language: "Abkhazian",
+  },
+  {
+    id: "2",
+    language: "Afar",
+  },
+  {
+    id: "3",
+    language: "Afrikaans",
+  },
+  {
+    id: "4",
+    language: "Akan",
+  },
+  {
+    id: "5",
+    language: "Albanian",
+  },
+  {
+    id: "6",
+    language: "Amharic",
+  },
+  {
+    id: "7",
+    language: "Arabic",
+  },
+  {
+    id: "8",
+    language: "Aragonese",
+  },
+  {
+    id: "9",
+    language: "Armenian",
+  },
+  {
+    id: "10",
+    language: "Assamese",
+  },
+  {
+    id: "11",
+    language: "Avaric",
+  },
+  {
+    id: "12",
+    language: "Avestan",
+  },
+  {
+    id: "13",
+    language: "Aymara",
+  },
+  {
+    id: "14",
+    language: "Azerbaijani",
+  },
+  {
+    id: "15",
+    language: "Azeri",
+  },
+  {
+    id: "16",
+    language: "Bambara",
+  },
+  {
+    id: "17",
+    language: "Bashkir",
+  },
+  {
+    id: "18",
+    language: "Basque",
+  },
+  {
+    id: "19",
+    language: "Belarusian",
+  },
+  {
+    id: "20",
+    language: "Bengali",
+  },
+  {
+    id: "21",
+    language: "Bihari",
+  },
+  {
+    id: "22",
+    language: "Bislama",
+  },
+  {
+    id: "23",
+    language: "Bosnian",
+  },
+  {
+    id: "24",
+    language: "Breton",
+  },
+  {
+    id: "25",
+    language: "Bulgarian",
+  },
+  {
+    id: "26",
+    language: "Burmese",
+  },
+  {
+    id: "27",
+    language: "Catalan",
+  },
+  {
+    id: "28",
+    language: "Chamorro",
+  },
+  {
+    id: "29",
+    language: "Chechen",
+  },
+  {
+    id: "30",
+    language: "Chichewa",
+  },
+  {
+    id: "31",
+    language: "Chinese",
+  },
+  {
+    id: "32",
+    language: "Chuvash",
+  },
+  {
+    id: "33",
+    language: "Cornish",
+  },
+  {
+    id: "34",
+    language: "Corsican",
+  },
+  {
+    id: "35",
+    language: "Cree",
+  },
+  {
+    id: "36",
+    language: "Croatian",
+  },
+  {
+    id: "37",
+    language: "Czech",
+  },
+  {
+    id: "38",
+    language: "Danish",
+  },
+  {
+    id: "39",
+    language: "Dhivehi",
+  },
+  {
+    id: "40",
+    language: "Dutch",
+  },
+  {
+    id: "41",
+    language: "Dzongkha",
+  },
+  {
+    id: "42",
+    language: "English",
+  },
+  {
+    id: "43",
+    language: "Esperanto",
+  },
+  {
+    id: "44",
+    language: "Estonian",
+  },
+  {
+    id: "45",
+    language: "Ewe",
+  },
+  {
+    id: "46",
+    language: "Faroese",
+  },
+  {
+    id: "47",
+    language: "Fijian",
+  },
+  {
+    id: "48",
+    language: "Finnish",
+  },
+  {
+    id: "49",
+    language: "French",
+  },
+  {
+    id: "50",
+    language: "Fulah",
+  },
+  {
+    id: "51",
+    language: "Gagauz",
+  },
+  {
+    id: "52",
+    language: "Galician",
+  },
+  {
+    id: "53",
+    language: "Ganda",
+  },
+  {
+    id: "54",
+    language: "Georgian",
+  },
+  {
+    id: "55",
+    language: "German",
+  },
+  {
+    id: "56",
+    language: "Greek",
+  },
+  {
+    id: "57",
+    language: "Greenlandic",
+  },
+  {
+    id: "58",
+    language: "Guarani",
+  },
+  {
+    id: "59",
+    language: "Gujarati",
+  },
+  {
+    id: "60",
+    language: "Haitian",
+  },
+  {
+    id: "61",
+    language: "Hausa",
+  },
+  {
+    id: "62",
+    language: "Hebrew",
+  },
+  {
+    id: "63",
+    language: "Herero",
+  },
+  {
+    id: "64",
+    language: "Hindi",
+  },
+  {
+    id: "65",
+    language: "Hiri Motu",
+  },
+  {
+    id: "66",
+    language: "Hungarian",
+  },
+  {
+    id: "67",
+    language: "Icelandic",
+  },
+  {
+    id: "68",
+    language: "Ido",
+  },
+  {
+    id: "69",
+    language: "Igbo",
+  },
+  {
+    id: "70",
+    language: "Indonesian",
+  },
+  {
+    id: "71",
+    language: "Interlingua",
+  },
+  {
+    id: "72",
+    language: "Interlingue",
+  },
+  {
+    id: "73",
+    language: "Inuktitut",
+  },
+  {
+    id: "74",
+    language: "Inupiaq",
+  },
+  {
+    id: "75",
+    language: "Irish Gaelic",
+  },
+  {
+    id: "76",
+    language: "Italian",
+  },
+  {
+    id: "77",
+    language: "Japanese",
+  },
+  {
+    id: "78",
+    language: "Javanese",
+  },
+  {
+    id: "79",
+    language: "Kannada",
+  },
+  {
+    id: "80",
+    language: "Kanuri",
+  },
+  {
+    id: "81",
+    language: "Kashmiri",
+  },
+  {
+    id: "82",
+    language: "Kazakh",
+  },
+  {
+    id: "83",
+    language: "Khasi",
+  },
+  {
+    id: "84",
+    language: "Khmer",
+  },
+  {
+    id: "85",
+    language: "Kikuyu",
+  },
+  {
+    id: "86",
+    language: "Kinyarwanda",
+  },
+  {
+    id: "87",
+    language: "Kirghiz",
+  },
+  {
+    id: "88",
+    language: "Komi",
+  },
+  {
+    id: "89",
+    language: "Kongo",
+  },
+  {
+    id: "100",
+    language: "Korean",
+  },
+  {
+    id: "101",
+    language: "Kurdish",
+  },
+  {
+    id: "102",
+    language: "Kwanyama",
+  },
+  {
+    id: "103",
+    language: "Lao",
+  },
+  {
+    id: "104",
+    language: "Latin",
+  },
+  {
+    id: "105",
+    language: "Latvian",
+  },
+  {
+    id: "106",
+    language: "Limburgish",
+  },
+  {
+    id: "107",
+    language: "Lingala",
+  },
+  {
+    id: "108",
+    language: "Lithuanian",
+  },
+  {
+    id: "109",
+    language: "Luba-Katanga",
+  },
+  {
+    id: "110",
+    language: "Luxembourgish",
+  },
+  {
+    id: "111",
+    language: "Macedonian",
+  },
+  {
+    id: "112",
+    language: "Malagasy",
+  },
+  {
+    id: "113",
+    language: "Malay",
+  },
+  {
+    id: "114",
+    language: "Malayalam",
+  },
+  {
+    id: "115",
+    language: "Maltese",
+  },
+  {
+    id: "116",
+    language: "Manx",
+  },
+  {
+    id: "117",
+    language: "Maori",
+  },
+  {
+    id: "118",
+    language: "Marathi",
+  },
+  {
+    id: "119",
+    language: "Marshallese",
+  },
+  {
+    id: "120",
+    language: "Mongolian",
+  },
+  {
+    id: "121",
+    language: "Montenegrin",
+  },
+  {
+    id: "122",
+    language: "Nauru",
+  },
+  {
+    id: "123",
+    language: "Navajo",
+  },
+  {
+    id: "124",
+    language: "Ndonga",
+  },
+  {
+    id: "125",
+    language: "North Ndebele",
+  },
+  {
+    id: "126",
+    language: "Nepali",
+  },
+  {
+    id: "127",
+    language: "Northern Sami",
+  },
+  {
+    id: "128",
+    language: "Norwegian",
+  },
+  {
+    id: "129",
+    language: "Occitan",
+  },
+  {
+    id: "130",
+    language: "Ojibwa",
+  },
+  {
+    id: "131",
+    language: "Oriya",
+  },
+  {
+    id: "132",
+    language: "Oromo",
+  },
+  {
+    id: "133",
+    language: "Ossetian",
+  },
+  {
+    id: "134",
+    language: "Pali",
+  },
+  {
+    id: "135",
+    language: "Panjabi",
+  },
+  {
+    id: "136",
+    language: "Pashto",
+  },
+  {
+    id: "137",
+    language: "Persian",
+  },
+  {
+    id: "138",
+    language: "Polish",
+  },
+  {
+    id: "139",
+    language: "Portuguese",
+  },
+  {
+    id: "140",
+    language: "Quechua",
+  },
+  {
+    id: "141",
+    language: "Romansh",
+  },
+  {
+    id: "142",
+    language: "Romanian",
+  },
+  {
+    id: "143",
+    language: "Russian",
+  },
+  {
+    id: "144",
+    language: "Samoan",
+  },
+  {
+    id: "145",
+    language: "Sango",
+  },
+  {
+    id: "146",
+    language: "Sanskrit",
+  },
+  {
+    id: "147",
+    language: "Sardinian",
+  },
+  {
+    id: "148",
+    language: "Scottish Gaelic",
+  },
+  {
+    id: "149",
+    language: "Serbian",
+  },
+  {
+    id: "150",
+    language: "Shona",
+  },
+  {
+    id: "151",
+    language: "Sichuan Yi",
+  },
+  {
+    id: "152",
+    language: "Sindhi",
+  },
+  {
+    id: "153",
+    language: "Sinhala",
+  },
+  {
+    id: "154",
+    language: "Slovak",
+  },
+  {
+    id: "155",
+    language: "Slovenian",
+  },
+  {
+    id: "156",
+    language: "Somali",
+  },
+  {
+    id: "157",
+    language: "Sotho",
+  },
+  {
+    id: "158",
+    language: "Spanish",
+  },
+  {
+    id: "159",
+    language: "Sundanese",
+  },
+  {
+    id: "160",
+    language: "Swahili",
+  },
+  {
+    id: "161",
+    language: "Swati",
+  },
+  {
+    id: "162",
+    language: "Swedish",
+  },
+  {
+    id: "163",
+    language: "Tagalog",
+  },
+  {
+    id: "164",
+    language: "Tahitian",
+  },
+  {
+    id: "165",
+    language: "Tajik",
+  },
+  {
+    id: "166",
+    language: "Tamil",
+  },
+  {
+    id: "167",
+    language: "Tatar",
+  },
+  {
+    id: "168",
+    language: "Telugu",
+  },
+  {
+    id: "169",
+    language: "Thai",
+  },
+  {
+    id: "170",
+    language: "Tibetan",
+  },
+  {
+    id: "171",
+    language: "Tigrinya",
+  },
+  {
+    id: "172",
+    language: "Tok Pisin",
+  },
+  {
+    id: "173",
+    language: "Tonga",
+  },
+  {
+    id: "174",
+    language: "Tsonga",
+  },
+  {
+    id: "175",
+    language: "Turkish",
+  },
+  {
+    id: "176",
+    language: "Turkmen",
+  },
+  {
+    id: "177",
+    language: "Twi",
+  },
+  {
+    id: "178",
+    language: "Uighur",
+  },
+  {
+    id: "179",
+    language: "Ukrainian",
+  },
+  {
+    id: "180",
+    language: "Urdu",
+  },
+  {
+    id: "181",
+    language: "Uzbek",
+  },
+  {
+    id: "182",
+    language: "Venda",
+  },
+  {
+    id: "183",
+    language: "Vietnamese",
+  },
+  {
+    id: "184",
+    language: "Volapük",
+  },
+  {
+    id: "185",
+    language: "Walloon",
+  },
+  {
+    id: "186",
+    language: "Welsh",
+  },
+  {
+    id: "187",
+    language: "Western Frisian",
+  },
+  {
+    id: "188",
+    language: "Wolof",
+  },
+  {
+    id: "189",
+    language: "Xhosa",
+  },
+  {
+    id: "190",
+    language: "Yiddish",
+  },
+  {
+    id: "191",
+    language: "Yoruba",
+  },
+  {
+    id: "192",
+    language: "Zhuang",
+  },
+  {
+    id: "193",
+    language: "Zulu",
+  },
+];
