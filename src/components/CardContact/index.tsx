7b6943907d1d13fdc377f8b6e8fa2ef8d12c88f6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-03 19:08:31 -0300

    :sparkles: feat: create of the function to edit a contact

--- src/components/CardContact/index.tsx
+++ src/components/CardContact/index.tsx
@@ -3,25 +3,39 @@ import editSVG from "../../assets/edit.svg";
 import deleteSVG from "../../assets/delete.svg";
 import { ButtonSmall } from "../../components/ButtonSmall";
 import { ListContactProps } from "../../pages/ListContacts";
+import { useContacts } from "../../context/Contact";
+import { useNavigate } from "react-router-dom";
 
 interface CardContactProps {
   data: ListContactProps;
 }
 
 export function CardContact({ data }: CardContactProps) {
+  const { deleteContact } = useContacts();
+  const navigate = useNavigate();
+
+  function handleEditContact() {
+    navigate(`/editform/${data.id}`);
+  }
+
   return (
     <div className={styles.cardContainer}>
-      <img src={data.photo} alt={data.name} className={styles.imageCard} />
+      <img src={data.photo} alt={data.firstname} className={styles.imageCard} />
       <strong className={styles.cardName}>
-        {data.name} {data.sobrenome}
+        {data.firstname} {data.lastname}
       </strong>
       <span className={styles.cardEmail}>{data.email}</span>
-      <span className={styles.cardBirthday}>{data.birthDay}</span>
+      <span className={styles.cardBirthday}>{data.birthday}</span>
       <span className={styles.cardGender}>{data.gender}</span>
       <span className={styles.cardLanguage}>{data.language}</span>
       <div className={styles.buttonCardContainer}>
         <ButtonSmall type="button" variant="edit">
-          <img src={editSVG} alt="edit icon" className={styles.imageButton} />
+          <img
+            src={editSVG}
+            alt="edit icon"
+            className={styles.imageButton}
+            onClick={handleEditContact}
+          />
           Editar
         </ButtonSmall>
         <ButtonSmall type="button" variant="cancel">
@@ -29,6 +43,7 @@ export function CardContact({ data }: CardContactProps) {
             src={deleteSVG}
             alt="delete icon"
             className={styles.imageButton}
+            onClick={() => deleteContact(data.id)}
           />
           Remover
         </ButtonSmall>
