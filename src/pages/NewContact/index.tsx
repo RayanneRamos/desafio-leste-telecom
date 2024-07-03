27fe33190a25d8d374452fbeb987c7e12d2dbc95
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-03 17:07:19 -0300

    :sparkles: feat: create a form validation in new contact page

--- src/pages/NewContact/index.tsx
+++ src/pages/NewContact/index.tsx
@@ -3,13 +3,65 @@ import logoImage from "../../assets/logo.png";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import { ButtonLarge } from "../../components/ButtonLarge";
-import { InputLarge } from "../../components/InputLarge";
-import { InputSmall } from "../../components/InputSmall";
 import { SelectMedium } from "../../components/SelectMedium";
 import { Link } from "react-router-dom";
 import { LANGUAGES } from "../../utils/languages";
+import { useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ErrorMessage } from "../../components/ErrorMessage";
+import { InputLarge } from "../../components/InputLarge";
+import { InputSmall } from "../../components/InputSmall";
+
+const languagesValues = LANGUAGES.map((language) => language.language);
+
+const NewContactSchema = z.object({
+  firstname: z
+    .string()
+    .min(3, { message: "O nome deve conter no mínimo 3 caracteres." }),
+  lastname: z
+    .string()
+    .min(3, { message: "O sobrenome deve conter no mínimo 3 caracteres." }),
+  email: z.string().email({ message: "O email é obrigatório." }),
+  photo: z.string().url({ message: "A imagem deve ser uma url do navegador." }),
+  birthday: z
+    .string()
+    .min(1, { message: "A data de aniversário é obrigatória." }),
+  gender: z.enum(["Masculino", "Feminino"], {
+    message: "O gênero é obrigatório.",
+  }),
+  language: z.enum(languagesValues as [string, ...string[]], {
+    message: "O idioma é obrigatório",
+  }),
+});
+
+type NewContactFormData = z.infer<typeof NewContactSchema>;
 
 export function NewContact() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NewContactFormData>({
+    resolver: zodResolver(NewContactSchema),
+  });
+
+  function handleSubmitAddNewContact(data: NewContactFormData) {
+    console.log(data);
+
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPhoto("");
+    setBirthday("");
+  }
+
   return (
     <div className={styles.container}>
       <Header />
@@ -21,48 +73,96 @@ export function NewContact() {
         />
       </Link>
       <h1 className={styles.title}>Adicionar um novo contato</h1>
-      <form className={styles.formContainer}>
+      <form
+        className={styles.formContainer}
+        onSubmit={handleSubmit(handleSubmitAddNewContact)}
+      >
         <div className={styles.formFirstLine}>
           <div className={styles.inputPrimaryContainer}>
             <label className={styles.label}>Nome:</label>
-            <InputLarge type="text" placeholder="John" />
+            <InputLarge
+              type="text"
+              placeholder="John"
+              value={firstName}
+              {...register("firstname")}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+            {errors.firstname && (
+              <ErrorMessage>{errors.firstname.message}</ErrorMessage>
+            )}
           </div>
           <div className={styles.inputPrimaryContainer}>
             <label className={styles.label}>Sobrenome:</label>
-            <InputLarge type="text" placeholder="Doe" />
+            <InputLarge
+              type="text"
+              placeholder="Doe"
+              value={lastName}
+              {...register("lastname")}
+              onChange={(event) => setLastName(event.target.value)}
+            />
+            {errors.lastname && (
+              <ErrorMessage>{errors.lastname.message}</ErrorMessage>
+            )}
           </div>
         </div>
         <div className={styles.formSecondLine}>
           <div className={styles.inputPrimaryContainer}>
             <label className={styles.label}>Email:</label>
-            <InputLarge type="text" placeholder="johndoe@example.com" />
+            <InputLarge
+              type="text"
+              placeholder="johndoe@example.com"
+              value={email}
+              {...register("email")}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            {errors.email && (
+              <ErrorMessage>{errors.email.message}</ErrorMessage>
+            )}
           </div>
           <div className={styles.inputPrimaryContainer}>
             <label className={styles.label}>Foto:</label>
             <InputLarge
               type="text"
               placeholder="https://github.com/RayanneRamos.png"
+              value={photo}
+              {...register("photo")}
+              onChange={(event) => setPhoto(event.target.value)}
             />
+            {errors.photo && (
+              <ErrorMessage>{errors.photo.message}</ErrorMessage>
+            )}
           </div>
         </div>
         <div className={styles.formThirdLine}>
           <div className={styles.inputSecondaryContainer}>
             <label className={styles.label}>Aniversário:</label>
-            <InputSmall type="date" placeholder="21/09/1997" />
+            <InputSmall
+              type="date"
+              placeholder="21/09/1997"
+              value={birthday}
+              {...register("birthday")}
+              onChange={(event) => setBirthday(event.target.value)}
+            />
+            {errors.birthday && (
+              <ErrorMessage>{errors.birthday.message}</ErrorMessage>
+            )}
           </div>
           <div className={styles.inputSecondaryContainer}>
             <label className={styles.label}>Gênero:</label>
-            <SelectMedium>
+            <SelectMedium defaultValue="" {...register("gender")}>
               <option value="" disabled selected>
                 Selecionar o gênero
               </option>
-              <option>Masculino</option>
-              <option>Feminino</option>
+              <option value="Masculino">Masculino</option>
+              <option value="Feminino">Feminino</option>
             </SelectMedium>
+            {errors.gender && (
+              <ErrorMessage>{errors.gender.message}</ErrorMessage>
+            )}
           </div>
           <div className={styles.inputSecondaryContainer}>
             <label className={styles.label}>Idioma:</label>
-            <SelectMedium>
+            <SelectMedium defaultValue="" {...register("language")}>
               <option value="" disabled selected>
                 Selecionar o idioma
               </option>
@@ -74,11 +174,14 @@ export function NewContact() {
                 );
               })}
             </SelectMedium>
+            {errors.language && (
+              <ErrorMessage>{errors.language.message}</ErrorMessage>
+            )}
           </div>
         </div>
         <div className={styles.buttonFormContainer}>
-          <ButtonLarge title="Adicionar" variant="confirm" />
-          <ButtonLarge title="Cancelar" variant="cancel" />
+          <ButtonLarge type="submit" title="Adicionar" variant="confirm" />
+          <ButtonLarge type="button" title="Cancelar" variant="cancel" />
         </div>
       </form>
       <Footer />
