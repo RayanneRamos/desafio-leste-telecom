e05cf72858a6a50f67485e3733bbf21357b0adcc
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-03 17:05:32 -0300

    :sparkles: feat: create a types

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface Contact {
+  id: string;
+  name: string;
+  sobrenome?: string;
+  email: string;
+  birthday: string;
+  language: string;
+  gender: string;
+  photo: string;
+}
