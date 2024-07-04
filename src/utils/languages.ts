8ac5b075cedc22ead95a2093b96772120fedf8d4
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-04 15:18:23 -0300

    :recycle: refactor: add values in mock

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -1,734 +1,917 @@
 export const LANGUAGES = [
   {
     id: "1",
+    value: "abkhazian",
     language: "Abkhazian",
   },
   {
     id: "2",
+    value: "afar",
     language: "Afar",
   },
   {
     id: "3",
+    value: "afrikaans",
     language: "Afrikaans",
   },
   {
     id: "4",
+    value: "akan",
     language: "Akan",
   },
   {
     id: "5",
+    value: "albanian",
     language: "Albanian",
   },
   {
     id: "6",
+    value: "amharic",
     language: "Amharic",
   },
   {
     id: "7",
+    value: "arabic",
     language: "Arabic",
   },
   {
     id: "8",
+    value: "aragonese",
     language: "Aragonese",
   },
   {
     id: "9",
+    value: "armenian",
     language: "Armenian",
   },
   {
     id: "10",
+    value: "assamese",
     language: "Assamese",
   },
   {
     id: "11",
+    value: "avaric",
     language: "Avaric",
   },
   {
     id: "12",
+    value: "avestan",
     language: "Avestan",
   },
   {
     id: "13",
+    value: "aymara",
     language: "Aymara",
   },
   {
     id: "14",
+    value: "azerbaijani",
     language: "Azerbaijani",
   },
   {
     id: "15",
+    value: "",
     language: "Azeri",
   },
   {
     id: "16",
+    value: "bambara",
     language: "Bambara",
   },
   {
     id: "17",
+    value: "bashkir",
     language: "Bashkir",
   },
   {
     id: "18",
+    value: "basque",
     language: "Basque",
   },
   {
     id: "19",
+    value: "belarusian",
     language: "Belarusian",
   },
   {
     id: "20",
+    value: "bengali",
     language: "Bengali",
   },
   {
     id: "21",
+    value: "bihari",
     language: "Bihari",
   },
   {
     id: "22",
+    value: "bislama",
     language: "Bislama",
   },
   {
     id: "23",
+    value: "bosnian",
     language: "Bosnian",
   },
   {
     id: "24",
+    value: "breton",
     language: "Breton",
   },
   {
     id: "25",
+    value: "bulgarian",
     language: "Bulgarian",
   },
   {
     id: "26",
+    value: "burmese",
     language: "Burmese",
   },
   {
     id: "27",
+    value: "catalan",
     language: "Catalan",
   },
   {
     id: "28",
+    value: "chamorro",
     language: "Chamorro",
   },
   {
     id: "29",
+    value: "chechen",
     language: "Chechen",
   },
   {
     id: "30",
+    value: "chichewa",
     language: "Chichewa",
   },
   {
     id: "31",
+    value: "chinese",
     language: "Chinese",
   },
   {
     id: "32",
+    value: "chuvash",
     language: "Chuvash",
   },
   {
     id: "33",
+    value: "cornish",
     language: "Cornish",
   },
   {
     id: "34",
+    value: "corsican",
     language: "Corsican",
   },
   {
     id: "35",
+    value: "cree",
     language: "Cree",
   },
   {
     id: "36",
+    value: "croatian",
     language: "Croatian",
   },
   {
     id: "37",
+    value: "czech",
     language: "Czech",
   },
   {
     id: "38",
+    value: "danish",
     language: "Danish",
   },
   {
     id: "39",
+    value: "dhivehi",
     language: "Dhivehi",
   },
   {
     id: "40",
+    value: "dutch",
     language: "Dutch",
   },
   {
     id: "41",
+    value: "dzongkha",
     language: "Dzongkha",
   },
   {
     id: "42",
+    value: "english",
     language: "English",
   },
   {
     id: "43",
+    value: "esperanto",
     language: "Esperanto",
   },
   {
     id: "44",
+    value: "estonian",
     language: "Estonian",
   },
   {
     id: "45",
+    value: "ewe",
     language: "Ewe",
   },
   {
     id: "46",
+    value: "faroese",
     language: "Faroese",
   },
   {
     id: "47",
+    value: "fijian",
     language: "Fijian",
   },
   {
     id: "48",
+    value: "finnish",
     language: "Finnish",
   },
   {
     id: "49",
+    value: "french",
     language: "French",
   },
   {
     id: "50",
+    value: "fulah",
     language: "Fulah",
   },
   {
     id: "51",
+    value: "gagauz",
     language: "Gagauz",
   },
   {
     id: "52",
+    value: "galician",
     language: "Galician",
   },
   {
     id: "53",
+    value: "ganda",
     language: "Ganda",
   },
   {
     id: "54",
+    value: "georgian",
     language: "Georgian",
   },
   {
     id: "55",
+    value: "german",
     language: "German",
   },
   {
     id: "56",
+    value: "greek",
     language: "Greek",
   },
   {
     id: "57",
+    value: "greenlandic",
     language: "Greenlandic",
   },
   {
     id: "58",
+    value: "guarani",
     language: "Guarani",
   },
   {
     id: "59",
+    value: "gujarati",
     language: "Gujarati",
   },
   {
     id: "60",
+    value: "haitian",
     language: "Haitian",
   },
   {
     id: "61",
+    value: "hausa",
     language: "Hausa",
   },
   {
     id: "62",
+    value: "hebrew",
     language: "Hebrew",
   },
   {
     id: "63",
+    value: "herero",
     language: "Herero",
   },
   {
     id: "64",
+    value: "hindi",
     language: "Hindi",
   },
   {
     id: "65",
+    value: "hirimotu",
     language: "Hiri Motu",
   },
   {
     id: "66",
+    value: "hungarian",
     language: "Hungarian",
   },
   {
     id: "67",
+    value: "icelandic",
     language: "Icelandic",
   },
   {
     id: "68",
+    value: "ido",
     language: "Ido",
   },
   {
     id: "69",
+    value: "igbo",
     language: "Igbo",
   },
   {
     id: "70",
+    value: "indonesian",
     language: "Indonesian",
   },
   {
     id: "71",
+    value: "interlingua",
     language: "Interlingua",
   },
   {
     id: "72",
+    value: "interlingue",
     language: "Interlingue",
   },
   {
     id: "73",
+    value: "inuktitut",
     language: "Inuktitut",
   },
   {
     id: "74",
+    value: "inupiaq",
     language: "Inupiaq",
   },
   {
     id: "75",
+    value: "irishgaelic",
     language: "Irish Gaelic",
   },
   {
     id: "76",
+    value: "italian",
     language: "Italian",
   },
   {
     id: "77",
+    value: "japanese",
     language: "Japanese",
   },
   {
     id: "78",
+    value: "javanese",
     language: "Javanese",
   },
   {
     id: "79",
+    value: "kannada",
     language: "Kannada",
   },
   {
     id: "80",
+    value: "kanuri",
     language: "Kanuri",
   },
   {
     id: "81",
+    value: "kashmiri",
     language: "Kashmiri",
   },
   {
     id: "82",
+    value: "kazakh",
     language: "Kazakh",
   },
   {
     id: "83",
+    value: "khasi",
     language: "Khasi",
   },
   {
     id: "84",
+    value: "khmer",
     language: "Khmer",
   },
   {
     id: "85",
+    value: "kikuyu",
     language: "Kikuyu",
   },
   {
     id: "86",
+    value: "kinyarwanda",
     language: "Kinyarwanda",
   },
   {
     id: "87",
+    value: "kirghiz",
     language: "Kirghiz",
   },
   {
     id: "88",
+    value: "komi",
     language: "Komi",
   },
   {
     id: "89",
+    value: "kongo",
     language: "Kongo",
   },
   {
     id: "100",
+    value: "korean",
     language: "Korean",
   },
   {
     id: "101",
+    value: "kurdish",
     language: "Kurdish",
   },
   {
     id: "102",
+    value: "kwanyama",
     language: "Kwanyama",
   },
   {
     id: "103",
+    value: "lao",
     language: "Lao",
   },
   {
     id: "104",
+    value: "latin",
     language: "Latin",
   },
   {
     id: "105",
+    value: "latvian",
     language: "Latvian",
   },
   {
     id: "106",
+    value: "limburgish",
     language: "Limburgish",
   },
   {
     id: "107",
+    value: "lingala",
     language: "Lingala",
   },
   {
     id: "108",
+    value: "lithuanian",
     language: "Lithuanian",
   },
   {
     id: "109",
+    value: "luba-katanga",
     language: "Luba-Katanga",
   },
   {
     id: "110",
+    value: "luxembourgish",
     language: "Luxembourgish",
   },
   {
     id: "111",
+    value: "macedonian",
     language: "Macedonian",
   },
   {
     id: "112",
+    value: "malagasy",
     language: "Malagasy",
   },
   {
     id: "113",
+    value: "malay",
     language: "Malay",
   },
   {
     id: "114",
+    value: "malayalam",
     language: "Malayalam",
   },
   {
     id: "115",
+    value: "maltese",
     language: "Maltese",
   },
   {
     id: "116",
+    value: "manx",
     language: "Manx",
   },
   {
     id: "117",
+    value: "maori",
     language: "Maori",
   },
   {
     id: "118",
+    value: "marathi",
     language: "Marathi",
   },
   {
     id: "119",
+    value: "marshallese",
     language: "Marshallese",
   },
   {
     id: "120",
+    value: "mongolian",
     language: "Mongolian",
   },
   {
     id: "121",
+    value: "montenegrin",
     language: "Montenegrin",
   },
   {
     id: "122",
+    value: "nauru",
     language: "Nauru",
   },
   {
     id: "123",
+    value: "navajo",
     language: "Navajo",
   },
   {
     id: "124",
+    value: "ndonga",
     language: "Ndonga",
   },
   {
     id: "125",
+    value: "northndebele",
     language: "North Ndebele",
   },
   {
     id: "126",
+    value: "nepali",
     language: "Nepali",
   },
   {
     id: "127",
+    value: "northernsami",
     language: "Northern Sami",
   },
   {
     id: "128",
+    value: "norwegian",
     language: "Norwegian",
   },
   {
     id: "129",
+    value: "occitan",
     language: "Occitan",
   },
   {
     id: "130",
+    value: "ojibwa",
     language: "Ojibwa",
   },
   {
     id: "131",
+    value: "oriya",
     language: "Oriya",
   },
   {
     id: "132",
+    value: "oromo",
     language: "Oromo",
   },
   {
     id: "133",
+    value: "ossetian",
     language: "Ossetian",
   },
   {
     id: "134",
+    value: "pali",
     language: "Pali",
   },
   {
     id: "135",
+    value: "panjabi",
     language: "Panjabi",
   },
   {
     id: "136",
+    value: "pashto",
     language: "Pashto",
   },
   {
     id: "137",
+    value: "persian",
     language: "Persian",
   },
   {
     id: "138",
+    value: "polish",
     language: "Polish",
   },
   {
     id: "139",
+    value: "portuguese",
     language: "Portuguese",
   },
   {
     id: "140",
+    value: "quechua",
     language: "Quechua",
   },
   {
     id: "141",
+    value: "romansh",
     language: "Romansh",
   },
   {
     id: "142",
+    value: "romanian",
     language: "Romanian",
   },
   {
     id: "143",
+    value: "russian",
     language: "Russian",
   },
   {
     id: "144",
+    value: "samoan",
     language: "Samoan",
   },
   {
     id: "145",
+    value: "sango",
     language: "Sango",
   },
   {
     id: "146",
+    value: "sanskrit",
     language: "Sanskrit",
   },
   {
     id: "147",
+    value: "sardinian",
     language: "Sardinian",
   },
   {
     id: "148",
+    value: "scottishgaelic",
     language: "Scottish Gaelic",
   },
   {
     id: "149",
+    value: "serbian",
     language: "Serbian",
   },
   {
     id: "150",
+    value: "shona",
     language: "Shona",
   },
   {
     id: "151",
+    value: "sichuanyi",
     language: "Sichuan Yi",
   },
   {
     id: "152",
+    value: "sindhi",
     language: "Sindhi",
   },
   {
     id: "153",
+    value: "sinhala",
     language: "Sinhala",
   },
   {
     id: "154",
+    value: "slovak",
     language: "Slovak",
   },
   {
     id: "155",
+    value: "slovenian",
     language: "Slovenian",
   },
   {
     id: "156",
+    value: "somali",
     language: "Somali",
   },
   {
     id: "157",
+    value: "sotho",
     language: "Sotho",
   },
   {
     id: "158",
+    value: "spanish",
     language: "Spanish",
   },
   {
     id: "159",
+    value: "sundanese",
     language: "Sundanese",
   },
   {
     id: "160",
+    value: "swahili",
     language: "Swahili",
   },
   {
     id: "161",
+    value: "swati",
     language: "Swati",
   },
   {
     id: "162",
+    value: "swedish",
     language: "Swedish",
   },
   {
     id: "163",
+    value: "tagalog",
     language: "Tagalog",
   },
   {
     id: "164",
+    value: "tahitian",
     language: "Tahitian",
   },
   {
     id: "165",
+    value: "tajik",
     language: "Tajik",
   },
   {
     id: "166",
+    value: "tamil",
     language: "Tamil",
   },
   {
     id: "167",
+    value: "tatar",
     language: "Tatar",
   },
   {
     id: "168",
+    value: "telugu",
     language: "Telugu",
   },
   {
     id: "169",
+    value: "thai",
     language: "Thai",
   },
   {
     id: "170",
+    value: "tibetan",
     language: "Tibetan",
   },
   {
     id: "171",
+    value: "tigrinya",
     language: "Tigrinya",
   },
   {
     id: "172",
+    value: "tokpisin",
     language: "Tok Pisin",
   },
   {
     id: "173",
+    value: "tonga",
     language: "Tonga",
   },
   {
     id: "174",
+    value: "tsonga",
     language: "Tsonga",
   },
   {
     id: "175",
+    value: "turkish",
     language: "Turkish",
   },
   {
     id: "176",
+    value: "turkmen",
     language: "Turkmen",
   },
   {
     id: "177",
+    value: "twi",
     language: "Twi",
   },
   {
     id: "178",
+    value: "uighur",
     language: "Uighur",
   },
   {
     id: "179",
+    value: "ukrainian",
     language: "Ukrainian",
   },
   {
     id: "180",
+    value: "urdu",
     language: "Urdu",
   },
   {
     id: "181",
+    value: "uzbek",
     language: "Uzbek",
   },
   {
     id: "182",
+    value: "venda",
     language: "Venda",
   },
   {
     id: "183",
+    value: "vietnamese",
     language: "Vietnamese",
   },
   {
     id: "184",
+    value: "volapük",
     language: "Volapük",
   },
   {
     id: "185",
+    value: "walloon",
     language: "Walloon",
   },
   {
     id: "186",
+    value: "welsh",
     language: "Welsh",
   },
   {
     id: "187",
+    value: "westernfrisian",
     language: "Western Frisian",
   },
   {
     id: "188",
+    value: "wolof",
     language: "Wolof",
   },
   {
     id: "189",
+    value: "xhosa",
     language: "Xhosa",
   },
   {
     id: "190",
+    value: "yiddish",
     language: "Yiddish",
   },
   {
     id: "191",
+    value: "yoruba",
     language: "Yoruba",
   },
   {
     id: "192",
+    value: "zhuang",
     language: "Zhuang",
   },
   {
     id: "193",
+    value: "zulu",
     language: "Zulu",
   },
 ];
--- src/utils/months.ts
+++ src/utils/months.ts
@@ -1,50 +1,62 @@
 export const MONTHS = [
   {
     id: "1",
+    value: "january",
     month: "January",
   },
   {
     id: "2",
+    value: "february",
     month: "February",
   },
   {
     id: "3",
+    value: "march",
     month: "March",
   },
   {
     id: "4",
+    value: "april",
     month: "April",
   },
   {
     id: "5",
+    value: "may",
     month: "May",
   },
   {
     id: "6",
+    value: "june",
     month: "June",
   },
   {
     id: "7",
+    value: "july",
     month: "July",
   },
   {
     id: "8",
+    value: "august",
     month: "August",
   },
   {
     id: "9",
+    value: "setptember",
     month: "September",
   },
   {
     id: "10",
+    value: "october",
     month: "October",
   },
   {
     id: "11",
+    value: "november",
     month: "November",
   },
   {
     id: "12",
+    value: "december",
     month: "December",
   },
 ];
