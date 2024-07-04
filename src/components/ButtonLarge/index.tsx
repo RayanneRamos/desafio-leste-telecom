bb7dfdef56ad05952f935514974f232b9a68e365
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-04 15:40:37 -0300

    :sparkles: feat: add new variant in component button

--- src/components/ButtonLarge/index.tsx
+++ src/components/ButtonLarge/index.tsx
@@ -2,7 +2,7 @@ import { ButtonHTMLAttributes } from "react";
 import styles from "./styles.module.scss";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "confirm" | "cancel";
+  variant?: "confirm" | "cancel" | "edit";
   title: string;
 }
 
@@ -13,6 +13,8 @@ export function ButtonLarge({ variant, title, ...props }: ButtonProps) {
         return styles.buttonConfirm;
       case "cancel":
         return styles.buttonCancel;
+      case "edit":
+        return styles.buttonEdit;
       default:
         return "";
     }
